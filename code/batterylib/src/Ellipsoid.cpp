57fc11544f8360d171a2f2842a7a4e9bd5d62399
krsvojte <vojtech.krs@outlook.com>
2017-11-13 20:29:13 -0500

    transform now affects volume & surface area of ellipsoid

--- code/batterylib/src/Ellipsoid.cpp
+++ code/batterylib/src/Ellipsoid.cpp
@@ -4,14 +4,16 @@ using namespace blib;
 
 const float pi = static_cast<float>(std::acos(-1.0));
 
-float blib::Ellipsoid::c() const { return param[0]; }
-float &blib::Ellipsoid::c() { return param[2]; }
+
 
 float blib::Ellipsoid::a() const { return param[0]; }
 float &blib::Ellipsoid::a() { return param[0]; }
 
 float blib::Ellipsoid::b() const { return param[1]; }
-float &blib::Ellipsoid::b() { return param[0]; }
+float &blib::Ellipsoid::b() { return param[1]; }
+
+float blib::Ellipsoid::c() const { return param[2]; }
+float &blib::Ellipsoid::c() { return param[2]; }
 
 Eigen::Vector3f blib::Ellipsoid::surfacePoint(float theta, float phi) const
 {
@@ -23,7 +25,10 @@ Eigen::Vector3f blib::Ellipsoid::surfacePoint(float theta, float phi) const
 }
 
 float blib::Ellipsoid::volume() const {
-	return (4.0f / 3.0f) * pi * a() * b() * c();
+	return (4.0f / 3.0f) * pi * 
+		a() * transform.scale[0] * 
+		b() * transform.scale[1] * 
+		c() * transform.scale[2];
 }
 
 float blib::Ellipsoid::surfaceAreaApproximation() const
@@ -33,9 +38,9 @@ float blib::Ellipsoid::surfaceAreaApproximation() const
 
 	const float p = 1.6075f;
 
-	auto ap = pow(a(), p);
-	auto bp = pow(b(), p);
-	auto cp = pow(c(), p);
+	auto ap = pow(a() * transform.scale[0], p);
+	auto bp = pow(b() * transform.scale[1], p);
+	auto cp = pow(c() * transform.scale[2], p);
 
 	return 4.0f * pi * pow(
 		(ap*bp + ap*cp + bp*cp) / 3.0f,
@@ -45,10 +50,10 @@ float blib::Ellipsoid::surfaceAreaApproximation() const
 }
 
 bool blib::Ellipsoid::isPointIn(const Eigen::Vector3f &pt) {
-  float v = pt[0] * pt[0] / (a() * a()) + pt[1] * pt[1] / (b() * b()) +
+	float v = pt[0] * pt[0] / (a() * a()) + pt[1] * pt[1] / (b() * b()) +
             pt[2] * pt[2] / (c() * c());
 
-  return v < 1.0f;
+	return v < 1.0f;
 }
 
 bool blib::Ellipsoid::isPointInGlobal(const Eigen::Vector3f &pt) {
