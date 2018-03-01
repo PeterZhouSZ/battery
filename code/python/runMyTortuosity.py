64d5285c429aa0019cc12a145581e6bfe9b42842
krsvojte <vojtech.krs@outlook.com>
2018-03-01 14:20:28 -0500

    More verbose tortuosity python script

--- code/python/runMyTortuosity.py
+++ code/python/runMyTortuosity.py
@@ -10,8 +10,15 @@ t0 = time.time()
 
 
 
-root = '../../data/graphiteSections/dataset1/'
+root = '../../data/graphiteSections/dataset2/'
 #f = open("output.csv",'w')
+
+print("Starting tau calculation for:")
+for subdir, dirs, files in os.walk(root):
+    if(subdir == root): continue    
+    print(subdir)
+
+sys.stdout.flush()
 
 for subdir, dirs, files in os.walk(root):
     if(subdir == root): continue
@@ -19,7 +26,7 @@ for subdir, dirs, files in os.walk(root):
 
     args = subdir + "/ -t -dneg --step 128 -v -o out.csv --volExport "# --sub 16 --volExport"
     print(args)
-
+    sys.stdout.flush()
     subprocess.call(
         '../bin/Release/batterytool.exe ' + args#,  stdout=f
     )
